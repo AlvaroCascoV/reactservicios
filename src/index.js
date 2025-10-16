83744805ede9aeff41a8e02922be86fb41d6d881
AlvaroCascoV <alvarocascovalero@gmail.com>
2025-10-16 13:36:07 +0200

    llamadas a apis

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import ServiciosApiCustomers from './components/ServiciosApiCustomers';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    
-  </React.StrictMode>
+  <ServiciosApiCustomers />
 );
 
 // If you want to start measuring performance in your app, pass a function
