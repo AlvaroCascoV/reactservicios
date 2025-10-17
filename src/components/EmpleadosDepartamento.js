4da2ef68383444f9bc8e3cf589435c35ca229ac7
AlvaroCascoV <alvarocascovalero@gmail.com>
2025-10-17 10:57:44 +0200

    funcion de seleccionar departamento

--- src/components/EmpleadosDepartamento.js
+++ src/components/EmpleadosDepartamento.js
@@ -3,14 +3,15 @@ import axios from 'axios';
 import Global from '../Global';
 
 export default class EmpleadosDepartamento extends Component {
-    url = Global.urlEmpleados;
-    cajaDepartamento = React.createRef();
+    urlEmpleados = Global.urlEmpleados;
+    urlDepartamentos = Global.urlDepartamentos;
+    selectDepartamento = React.createRef();
 
     buscarEmpleados = (event) => {
         event.preventDefault();
-        let idDepartamento = this.cajaDepartamento.current.value;
+        let idDepartamento = this.selectDepartamento.current.value;
         let request = "api/Empleados/empleadosdepartamento/" + idDepartamento
-        axios.get(this.url + request).then(response => {
+        axios.get(this.urlEmpleados + request).then(response => {
             console.log("leyendo empleados con idDepartemento: " + idDepartamento)
             this.setState({
                 empleados: response.data
@@ -18,16 +19,38 @@ export default class EmpleadosDepartamento extends Component {
         })
     }
 
+    loadDepartamentos = () => {
+        let request = "webresources/departamentos"
+        axios.get(this.urlDepartamentos + request).then(response => {
+            console.log("Cargando departamentos")
+            this.setState({
+                departamentos: response.data
+            })
+        })
+    }
+
+    componentDidMount = () => {
+        this.loadDepartamentos();
+    }
+
     state = {
-        empleados: []
+        empleados: [],
+        departamentos: []
     }
 
     render() {
         return (<div>
             <h1 style={{ color: "blue" }}>Api Empleados Departamento</h1>
             <form>
-                <label>Introduzca ID Departamento: </label>
-                <input type="text" ref={this.cajaDepartamento} />
+                <label>Seleccione Departamento: </label>
+                <select ref={this.selectDepartamento}>
+                    <option />
+                    {
+                        this.state.departamentos.map((departamento, index) => {
+                            return (<option key={index} value={departamento.numero}>{departamento.nombre}</option>)
+                        })
+                    }
+                </select>
                 <button onClick={this.buscarEmpleados}>Buscar empleados</button>
             </form>
             <ul>
